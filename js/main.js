fe8b3a0a73450d804b6dc8e067824c54a61cff2b
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-05-17 15:44:22 -0300

    add:Local storage

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
 const form = document.querySelector('#novoItem')
-const lista = document.querySelector('#lista')
+const list = document.querySelector('#lista')
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach(element => {
+  criaElemento(element)
+});
 
 form.addEventListener('submit', (event) => {
+  const nome = event.target.elements['nome'];
+  const qtd = event.target.elements['quantidade'];
+  const itemAtual = {
+    "nome": nome.value,
+    "qtd": qtd.value
+  }
   event.preventDefault()
-  criaElemento(event.target.elements['nome'].value, event.target.elements['quantidade'].value)
+  criaElemento(itemAtual)
+
+  itens.push(itemAtual)
+  localStorage.setItem("itens", JSON.stringify(itens))
+
+  limparForm(nome.value = '', qtd.value = '')
+
 })
 
-function criaElemento(nome, qtd) {
-  //criação do novo elemento da lista com um class item
+function criaElemento(item) {
   const novoItem = document.createElement('li')
-  novoItem.classList.add("item")
-  //criação da tag strong com o parâmetro qtd dentro
-  const numeroItem = document.createElement('strong')
-  numeroItem.innerHTML = qtd
-  //aglutinação do novo elemento com a tag strong e o parâmetro nome
-  novoItem.appendChild(numeroItem)
-  novoItem.innerHTML += nome
+  novoItem.classList.add('item')
+  
+  const numero = document.createElement('strong')
+  novoItem.appendChild(numero)
+  
+  numero.innerHTML = item.qtd
+  novoItem.innerHTML += item.nome
+  
+  list.appendChild(novoItem)
 
-  //local aonde será alocado o novo elemento, vulgo, <ul id="lista">...</ul>.
-  lista.appendChild(novoItem)
+}
 
-  console.log(lista)
-
+function limparForm() {
 }
