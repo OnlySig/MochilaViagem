5a09a8e9424398aaaee9a5a7abc6024dd72f269c
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-05-19 14:58:40 -0300

    add: keyId, delete, atualizaElemento

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const form = document.querySelector('#novoItem')
 const list = document.querySelector('#lista')
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+const remover = document.querySelector('#remove')
+
+remover.addEventListener('click', () =>{
+  localStorage.clear()
+  location.reload() //recarrega a página automaticamente
+})
 
 itens.forEach(element => {
   criaElemento(element)
@@ -9,14 +15,24 @@ itens.forEach(element => {
 form.addEventListener('submit', (event) => {
   const nome = event.target.elements['nome'];
   const qtd = event.target.elements['quantidade'];
+  const existe = itens.find(element => element.nome === nome.value)
+
   const itemAtual = {
     "nome": nome.value,
     "qtd": qtd.value
   }
   event.preventDefault()
-  criaElemento(itemAtual)
-
-  itens.push(itemAtual)
+  if(nome.value === '' || qtd.value === '' || qtd.value <= 0) {
+  alert('[ERRO] valores invalidos')
+  } else if(existe) {
+    itemAtual.id = existe.id
+    atualizaElemento(itemAtual)
+    itens[existe.id] = itemAtual
+  } else {
+    itemAtual.id = itens.length
+    criaElemento(itemAtual)
+    itens.push(itemAtual)
+  }
   localStorage.setItem("itens", JSON.stringify(itens))
 
   limparForm(nome.value = '', qtd.value = '')
@@ -29,12 +45,24 @@ function criaElemento(item) {
   
   const numero = document.createElement('strong')
   novoItem.appendChild(numero)
+  numero.dataset.id = item.id
   
   numero.innerHTML = item.qtd
   novoItem.innerHTML += item.nome
   
+  novoItem.appendChild(botaoDeleta())
   list.appendChild(novoItem)
 
+}
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='"+item.id+"']").innerHTML = item.qtd
+}
+
+function botaoDeleta() {
+  const elementoBotao = document.createElement("button")
+  elementoBotao.innerHTML = "X"
+  return elementoBotao;
 }
 
 function limparForm() {
